f217208131e2a984e64fb966c91a007cd791e0ee
Courtskit <Courtskit@gmail.com>
2020-05-30 23:53:45 -0700

    snow affect

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ window.onload = function () {
   // JAVASCRIPT TRICK #1 - Snow background with Canvas
 
 
-
   // get canvas and context.. store it vars
-  const canvas = $('#sky');
-  const ctx = canvas.getContext('2d');
+  var canvas = document.getElementById('sky');
+  var ctx = canvas.getContext('2d');
 
   // set canvas dims to window height and width
-  let w = window.innerWidth;
-  let h = window.innerHeight;
+  var w = window.innerWidth;
+  var h = window.innerHeight;
   canvas.width = w;
   canvas.height = h;
 
@@ -20,20 +19,20 @@ window.onload = function () {
 
   // loop through empty flakes and apply attributes
 
-  for (let i = 0; i < mf; i++) {
+  for (var i = 0; i < mf; i++) {
     flakes.push({
       x: Math.random() * w,
       y: Math.random() * h,
-      r: Math.random() * 5 + 2,
+      r: Math.random() * 10 + 2,
       d: Math.random() + 1
     })
   }
   // draw flakes onto canvas
   function drawFlakes() {
     ctx.clearRect(0, 0, w, h);
-    ctx.fillStyle = 'blue';
-    ctx.beginPath():
-    for (let i = 0; i < mf: i++) {
+    ctx.fillStyle = "cyan";
+    ctx.beginPath();
+    for (var i = 0; i < mf; i++) {
       var f = flakes[i];
       ctx.moveTo(f.x, f.y);
       ctx.arc(f.x, f.y, f.r, 0, Math.PI * 2, true);
@@ -41,4 +40,27 @@ window.onload = function () {
     ctx.fill();
     moveFlakes();
   }
+  // animate the flakes
+  var angle = 0;
+
+  function moveFlakes() {
+    angle += 0.01;
+    for (var i = 0; i < mf; i++) {
+      // store current flake
+      var f = flakes[i];
+
+      // update x and y coordinates of each snowflake
+      f.y += Math.pow(f.d, 2) + 1;
+      f.x += Math.sin(angle) * 2;
+
+      // if the snow reaches the bottom, send a new one to the top
+
+      if (f.y > h) {
+        flakes[i] = { x: Math.random() * w, y: 0, r: f.r, d: f.d };
+      }
+    }
+  }
+  setInterval(drawFlakes, 25);
 }
+
+
